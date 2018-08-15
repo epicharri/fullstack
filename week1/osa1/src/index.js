f168be521697b8d5fe4f86aa87f6c3b8b0f22ac3
epicharri <harri.kahkonen3@gmail.com>
2018-08-15 14:20:33 +0300

    Tehtävä 1.3

--- week1/osa1/src/index.js
+++ week1/osa1/src/index.js
@@ -3,19 +3,24 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     
-
     const kurssi = 'Half Stack -sovelluskehitys'
-    const osa1 = 'Reactin perusteet'
-    const tehtavia1 = 10
-    const osa2 = 'Tiedonvälitys propseilla'
-    const tehtavia2 = 7
-    const osa3 = 'Komponenttien tila'
-    const tehtavia3 = 14
+    const osa1 = {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+    }
+    const osa2 = {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+    }
+    const osa3 = {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+    }
 
     const Osa = (props) => {
         return (
             <div>
-                <p>{props.osa} {props.tehtavia}</p>
+                <p>{props.osa.nimi} {props.osa.tehtavia}</p>
             </div>
         )
     }
@@ -32,9 +37,9 @@ const App = () => {
     const Sisalto = (props) => {
         return (
             <div>
-                <Osa osa={osa1} tehtavia={tehtavia1} />
-                <Osa osa={osa2} tehtavia={tehtavia2} />
-                <Osa osa={osa3} tehtavia={tehtavia3} />
+                <Osa osa={osa1} />
+                <Osa osa={osa2} />
+                <Osa osa={osa3} />
             </div>
         )
     }
@@ -42,7 +47,7 @@ const App = () => {
     const Yhteensa = (props) => {
         return (
             <div>
-                <p>yhteensä {props.tehtavia1 + props.tehtavia2 + props.tehtavia3} tehtävää</p>
+                <p>yhteensä {props.osa1.tehtavia + props.osa2.tehtavia + props.osa3.tehtavia} tehtävää</p>
             </div>
         )
     }
@@ -51,9 +56,8 @@ const App = () => {
     return (
         <div>
             <Otsikko kurssi={kurssi} />
-            <Sisalto osa1={osa1} osa2={osa2} osa3={osa3}
-                tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3} />
-            <Yhteensa tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3} />
+            <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} />
+            <Yhteensa osa1={osa1} osa2={osa2} osa3={osa3} />
         </div>
     )
 }
